7efbe82e2d0a7bf67eb17344436acaa2bd349a77
Niraj Kumar Verma <vermaniraj9607@gmail.com>
2025-08-01 16:55:58 +0530

    Add files via upload

---
+++ 9. Sorting/BubbleSort.java
@@ -0,0 +1,28 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]){
+        for(int turn =0; turn < arr.length-1; turn++){
+            for(int j=0; j< arr.length-1-turn; j++){
+                if(arr[j] > arr[j+1]){
+                    // Swap
+
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]){
+        for(int i =0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 1, 5, 2};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
---
+++ 9. Sorting/SelectionSort.java
@@ -0,0 +1,10 @@
+public class SelectionSort {
+
+    public static void selectionSort(int arr[]){
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 1, 5, 2};
+        
+    }
+}
