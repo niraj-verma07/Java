ab75c86a4045de75c9680a6554876fd0d8cf508b
Niraj Kumar Verma <vermaniraj9607@gmail.com>
2025-08-19 22:22:34 +0530

    Add files via upload

--- 13. OOPS/OOPS.java
+++ 13. OOPS/OOPS.java
@@ -1,33 +1,46 @@
-public class OOPS{
+public class OOPS {
     public static void main(String[] args) {
-        Pen p1 = new Pen(); //Created a pen object name p1
+        Pen p1 = new Pen(); // Created a pen object name p1
         p1.setColor("blue");
         // p1.color = "Yellow"; //Another way to change color
         System.out.println(p1.color);
         p1.setTip(5);
         System.out.println(p1.tip);
+
+        BankAccount myAcc = new BankAccount();
+        myAcc.username = "niraj";
+        myAcc.setPassword("abcdefg");
     }
 }
 
-class Pen{
+class BankAccount {
+    public String username;
+    private String password;
+
+    void setPassword(String pwd) { 
+        password = pwd;
+    }
+}
+
+class Pen {
     String color;
     int tip;
 
-    void setColor(String newColor){
+    void setColor(String newColor) {
         color = newColor;
     }
 
-    void setTip(int newTip){
+    void setTip(int newTip) {
         tip = newTip;
     }
 }
 
-class Student{
+class Student {
     String name;
     int age;
     float percentage;
 
-    void calcPercentage(int phy, int chem, int math){
-        percentage = (phy+chem+math)/3;
+    void calcPercentage(int phy, int chem, int math) {
+        percentage = (phy + chem + math) / 3;
     }
 }
