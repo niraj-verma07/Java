076771d30c05a3f63c5968ffb5b16708dc81d62b
niraj-verma07 <vermaniraj9607@gmail.com>
2025-08-27 16:37:36 +0530

    commit

--- 13. OOPS/Polymor.java
+++ 13. OOPS/Polymor.java
@@ -1,5 +1,43 @@
 class Polymor{
-    public class static void (String args[]){
-        
+    public static void main(String[] args) {
+        //Method overloading
+        Calculator cal = new Calculator();
+        System.out.println(cal.sum(10,20));
+        System.out.println(cal.sum((float)1.5,(float)2.5));
+        System.out.println(cal.sum(10,20,30));
+
+        //Method overriding
+
+        Deer d = new Deer();
+        d.eat();
+    }
+}
+
+//Method overloading
+class Calculator{
+    int sum(int a, int b){
+        return a+b;
+    }
+
+    float sum(float a, float b){
+        return a + b;
+    }
+
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+}
+
+//Method overriding
+
+class Animal{
+    void eat(){
+        System.out.println("eats anything");
+    }
+}
+
+class Deer extends Animal{
+    void eat(){
+        System.out.println("Eats grass");
     }
 }
