b51179c3b9a35c6c2060890f3cd5013a86a843f2
niraj-verma07 <vermaniraj9607@gmail.com>
2025-09-06 19:45:57 +0530

    Tiling Problem

--- 14. Recursion/Tiling.java
+++ 14. Recursion/Tiling.java
@@ -1,5 +1,24 @@
 public class Tiling {
+
+    public static int tilingProblem(int n) {
+        // Base Case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        // Vertical Choice
+        int fnm1 = tilingProblem(n - 1);
+
+        // Horizontal Choice
+        int fnm2 = tilingProblem(n - 2);
+
+        // Total Ways
+        int totalWays = fnm1 + fnm2;
+
+        return totalWays;
+    }
+
     public static void main(String[] args) {
-        System.out.println("null");
+        System.out.println(tilingProblem(4));
     }
 }
